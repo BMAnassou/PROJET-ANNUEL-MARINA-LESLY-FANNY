318236636ec5e7b843d673f4b9e1284d6a9077c5
lesly <lesly97420@gmail.com>
2024-07-15 23:12:37 +0200

    Add first test enemy waves + retouches

---
+++ Assets/Script/ButtonInfo.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonInfo : MonoBehaviour
+{
+    public int itemID;
+    public Text priceTxt;
+    public GameObject shopManager;
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Script/SinglePlayerMode/EnemySpawner.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+   /* [System.Serializable] 
+    public class WaveContent
+    {
+        [SerializeField][NonReorderable] GameObject[] enemySpawn;
+
+        public GameObject GetEnemySpawnList()
+        {
+            return enemySpawn;
+        }
+    }
+    [SerializeField][NonReorderable] WaveContent[] waves;
+
+    private int currentWave = 0;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void SpawnWave()
+    {
+        for (int i = 0; i < waves[currentWave].GetEnemySpawnList().Length; i++)
+        {
+            
+        }
+    }*/
+}
